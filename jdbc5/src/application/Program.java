baf31ccbc58baf3b8adfb2ea55cf0af48da0309e
Israel Felix <israelfelixbenny@gmail.com>
2023-03-02 15:26:29 -0300

    atualização

--- jdbc5/src/application/Program.java
+++ jdbc5/src/application/Program.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import db.DB;
+import db.DbIntegrityException;
 
 public class Program {
 
@@ -16,20 +17,18 @@ public class Program {
 			conn = DB.getConnection();
 	
 			st = conn.prepareStatement(
-					"UPDATE seller "
-					+ "SET BaseSalary = BaseSalary + ? "
+					"DELETE FROM department "
 					+ "WHERE "
-					+ "(DepartmentId = ?)");
+					+ "Id = ?");
 
-			st.setDouble(1, 200.0);
-			st.setInt(2, 2);
+			st.setInt(1, 5);
 			
 			int rowsAffected = st.executeUpdate();
 			
 			System.out.println("Done! Rows affected: " + rowsAffected);
 		}
 		catch (SQLException e) {
-			e.printStackTrace();
+			throw new DbIntegrityException(e.getMessage());
 		} 
 		finally {
 			DB.closeStatement(st);
